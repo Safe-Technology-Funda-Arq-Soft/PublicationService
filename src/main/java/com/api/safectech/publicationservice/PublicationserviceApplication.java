2a51b90d33e566e74d3dca33be2c1e5aabf7fabf
Debie <debiegarcia@hotmail.com>
2022-05-22 20:57:33 -0500

    Add Eureka Client

--- src/main/java/com/api/safectech/publicationservice/PublicationserviceApplication.java
+++ src/main/java/com/api/safectech/publicationservice/PublicationserviceApplication.java
@@ -3,11 +3,13 @@ package com.api.safectech.publicationservice;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class PublicationserviceApplication {
 
 	public static void main(String[] args) {
